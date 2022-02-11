f4b918cc2c6819b9ce86510f5e127c542f73df1e
sik <ksilbermann@gmx.net>
2022-02-11 21:52:32 +0100

    Move lexing into constructors

--- Sources/GetSchwifty/Lexer.swift
+++ Sources/GetSchwifty/Lexer.swift
@@ -11,22 +11,114 @@ internal extension StringLexeme {
     }
 }
 
-private struct NewlineLex: Lexeme {}
-private struct WhitespaceLex: Lexeme {}
-private struct DelimiterLex: Lexeme {}
+private struct NewlineLex: Lexeme {
+    init(_ lexemes: inout [Lexeme]) {
+        lexemes.append(self)
+    }
+}
+
+private struct WhitespaceLex: Lexeme {
+    init(_ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
+        while chars.peek()?.isWhitespace ?? false {
+            _ = chars.pop()
+        }
+        lexemes.append(self)
+    }
+}
+
+private struct DelimiterLex: Lexeme {
+    init(_ lexemes: inout [Lexeme]) {
+        lexemes.append(self)
+    }
+}
+
 private struct CommentLex: StringLexeme {
     var string_rep: String = ""
+
+    init(_ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
+        var depth = 0
+
+        while let c = chars.pop() {
+            if c == "(" {
+                depth += 1
+            } else if c == ")" {
+                if depth == 0 {
+                    break
+                }
+                depth -= 1
+            }
+            self.push(c)
+        }
+
+        lexemes.append(self)
+    }
 }
+
 private struct StringLex: StringLexeme {
     var string_rep: String = ""
+    init(_ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
+        while let c = chars.pop() {
+            if c == "\\" {
+                self.push(c)
+                self.push(chars.pop()!)
+                continue
+            } else if c == "\"" {
+                break
+            }
+            self.push(c)
+        }
+
+        lexemes.append(self)
+    }
 }
+
 private struct WordLex: StringLexeme {
     var string_rep: String = ""
+
+    init(firstChar: Character, _ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
+        self.push(firstChar)
+        while chars.peek()?.isLetter ?? false {
+            self.push(chars.pop()!)
+        }
+        lexemes.append(self)
+    }
 }
+
 internal struct NumberLex: StringLexeme {
     var string_rep: String = ""
     var float_rep: Float {
         return Float(string_rep)!
+    }
+
+    fileprivate init(firstChar: Character, _ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
+        self.push(firstChar)
+
+        var accept_decimal_point = firstChar != "."
+        var accept_exp = firstChar.isNumber
+        var accept_sign = false
+
+        while let c = chars.peek() {
+            if c.isNumber {
+                accept_exp = true
+                accept_sign = false
+            } else if accept_decimal_point && c == "." {
+                accept_decimal_point = false
+                accept_exp = false
+            } else if accept_exp && c.lowercased() == "e" {
+            accept_decimal_point = false
+            accept_sign = true
+            accept_exp = false
+            } else if accept_sign && (c == "-" || c == "+") {
+            accept_sign = false
+            } else {
+            break
+            }
+
+            self.push(c)
+            _ = chars.pop()
+        }
+
+        lexemes.append(self)
     }
 }
 
@@ -43,119 +135,27 @@ private struct StringFifo {
     }
 }
 
-private func lex_comment(_ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
-    var depth = 0
-    var comment = CommentLex()
-
-    while let c = chars.pop() {
-        if c == "(" {
-            depth += 1
-        } else if c == ")" {
-            if depth == 0 {
-                break
-            }
-            depth -= 1
-        }
-        comment.push(c)
-    }
-
-    lexemes.append(comment)
-}
-
-private func lex_string(_ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
-    var string = StringLex()
-
-    while let c = chars.pop() {
-        if c == "\\" {
-            string.push(c)
-            string.push(chars.pop()!)
-            continue
-        } else if c == "\"" {
-            break
-        }
-        string.push(c)
-    }
-
-    lexemes.append(string)
-}
-
-private func lex_newline(_ lexemes: inout [Lexeme]) {
-    lexemes.append(NewlineLex())
-}
-
-private func lex_delimiter(_ lexemes: inout [Lexeme]) {
-    lexemes.append(DelimiterLex())
-}
-
-private func lex_whitespace(_ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
-    while chars.peek()?.isWhitespace ?? false {
-        _ = chars.pop()
-    }
-    lexemes.append(WhitespaceLex())
-}
-
-private func lex_word(firstChar: Character, _ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
-    var word = WordLex()
-    word.push(firstChar)
-    while chars.peek()?.isLetter ?? false {
-        word.push(chars.pop()!)
-    }
-    lexemes.append(word)
-}
-
-private func lex_number(firstChar: Character, _ lexemes: inout [Lexeme], _ chars: inout StringFifo) {
-    var num = NumberLex()
-    num.push(firstChar)
-
-    var accept_decimal_point = firstChar != "."
-    var accept_exp = firstChar.isNumber
-    var accept_sign = false
-
-    while let c = chars.peek() {
-        if c.isNumber {
-            accept_exp = true
-            accept_sign = false
-        } else if accept_decimal_point && c == "." {
-            accept_decimal_point = false
-            accept_exp = false
-        } else if accept_exp && c.lowercased() == "e" {
-           accept_decimal_point = false
-           accept_sign = true
-           accept_exp = false
-        } else if accept_sign && (c == "-" || c == "+") {
-           accept_sign = false
-        } else {
-           break
-        }
-
-        num.push(c)
-        _ = chars.pop()
-    }
-
-    lexemes.append(num)
-}
-
 internal func lex(_ inp: String) -> [Lexeme] {
     var lexemes: [Lexeme] = []
     var chars = StringFifo(inp)
 
     while let c = chars.pop() {
         if c == "(" {
-            lex_comment(&lexemes, &chars)
+            CommentLex(&lexemes, &chars)
         } else if c == "\"" {
-            lex_string(&lexemes, &chars)
+            StringLex(&lexemes, &chars)
         } else if c == "\r" {
             continue
         } else if c == "\n" || c == "\r\n" {
-            lex_newline(&lexemes)
+            NewlineLex(&lexemes)
         } else if c.isWhitespace {
-            lex_whitespace(&lexemes, &chars)
+            WhitespaceLex(&lexemes, &chars)
         } else if c.isLetter {
-            lex_word(firstChar: c, &lexemes, &chars)
+            WordLex(firstChar: c, &lexemes, &chars)
         } else if c.isNumber || c == "+" || c == "-" || c == "." {
-            lex_number(firstChar: c, &lexemes, &chars)
+            NumberLex(firstChar: c, &lexemes, &chars)
         } else if c == "," || c == "&" {
-            lex_delimiter(&lexemes)
+            DelimiterLex(&lexemes)
         } else {
             assertionFailure("Found unlexable chars at end of inp")
         }
