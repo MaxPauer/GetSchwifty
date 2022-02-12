ce77f04edf7bff295ba8b196f3dc539e94cee88f
sik <ksilbermann@gmx.net>
2022-02-12 11:01:17 +0100

    Add parsing for common variables

--- Sources/GetSchwifty/Parser.swift
+++ Sources/GetSchwifty/Parser.swift
@@ -1,10 +1,52 @@
 internal protocol Expr {}
 
+internal protocol VariableName: Expr {
+    var name: String { get }
+}
+
+internal struct CommonVariableName: VariableName {
+    var name: String
+}
+
+typealias Lexemes = Fifo<[Lexeme]>
+extension String: Error {}
+
 internal struct Parser {
     var lines: UInt = 1
+    var exprs: [Expr] = []
+
+    func parse_common_variable(_ lexemes: inout Lexemes, firstWord: String) throws -> Expr {
+        guard let ws = lexemes.pop() else {
+            throw "UnexpectedEOFError(expected: .whitespace)"
+        }
+        guard ws == .whitespace else {
+            throw "UnexpectedLexemeError(ws, expected: .whitespace)"
+        }
+
+        guard let sw = lexemes.pop() else {
+            throw "UnexpectedEOFError(expected: .word)"
+        }
+        guard case .word(let secWord) = sw else {
+            throw "UnexpectedLexemeError(sw, expected: .word)"
+        }
+
+        let secondWord = secWord.lowercased()
+        return CommonVariableName(name: "\(firstWord) \(secondWord)")
+    }
+
+    func parse_word(_ lexemes: inout Lexemes, firstWord: String) throws -> Expr? {
+        let first = firstWord.lowercased()
+        switch first {
+        case "a", "an", "the", "my", "your", "our":
+            return try parse_common_variable(&lexemes, firstWord: first)
+        default:
+            //throw "UnparsableWordError(word: firstWord)"
+            return nil
+        }
+    }
 
     init(lexemes: [Lexeme]) {
-        var lexs = Fifo<[Lexeme]>(lexemes)
+        var lexs = Lexemes(lexemes)
 
         while let l = lexs.pop() {
             switch l {
@@ -12,6 +54,10 @@ internal struct Parser {
                 lines += 1
             case .comment(_, let l):
                 lines += l
+            case .word(let w):
+                if let e = try? parse_word(&lexs, firstWord: w) {
+                    exprs.append(e)
+                }
             default:
                 continue
             }
--- Tests/GetSchwiftyTests/Parser.swift
+++ Tests/GetSchwiftyTests/Parser.swift
@@ -2,6 +2,21 @@ import XCTest
 @testable import GetSchwifty
 
 final class ParserTests: XCTestCase {
+    func testCommonVariable() throws {
+        let testParse = { (inp: String, exp: String) in
+            let p = Parser(lexemes: lex(inp))
+            XCTAssertEqual(p.exprs.count, 1)
+            XCTAssertEqual((p.exprs[0] as! CommonVariableName).name, exp)
+        }
+
+        testParse("A horse", "a horse")
+        testParse("An  elf", "an elf")
+        testParse("THE HOUSE", "the house")
+        testParse("My\tlife", "my life")
+        testParse("your Heart", "your heart")
+        testParse("our SoCiEtY", "our society")
+    }
+
     func testFizzBuzz() throws {
         let fizzbuzz = try! String(contentsOf: URL(fileURLWithPath: "./Tests/fizzbuzz.rock"))
         let lexemes = lex(fizzbuzz)
