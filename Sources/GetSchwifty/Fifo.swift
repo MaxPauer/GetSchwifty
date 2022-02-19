77e0df76d86e5f2edfa56bc91293ea0a3df7fd93
sik <ksilbermann@gmx.net>
2022-02-19 13:42:41 +0100

    Add double linked list implementation
    I know Swift.collections and Deque exists, but come on.. this was fun :D
    Also, I really want to do this without third party requirements,
    if possible.

--- Sources/GetSchwifty/Fifo.swift
+++ Sources/GetSchwifty/Fifo.swift
@@ -1,3 +1,71 @@
+internal struct DLinkedList<T> {
+    private class LLNode<T> {
+        let value: T
+        var previous: LLNode?
+        var next: LLNode?
+
+        init(_ v: T) {
+            value = v
+        }
+    }
+
+    private var front: LLNode<T>?
+    private var back: LLNode<T>?
+
+    mutating func pushFront(_ v: T?) {
+        guard let v = v else { return }
+        let node = LLNode(v)
+        if front == nil {
+            front = node
+            back = node
+        } else {
+            front!.previous = node
+            node.next = front
+            front = node
+        }
+    }
+
+    mutating func pushBack(_ v: T?) {
+        guard let v = v else { return }
+        let node = LLNode(v)
+        if back == nil {
+            back = node
+            front = node
+        } else {
+            back!.next = node
+            node.previous = back
+            back = node
+        }
+    }
+
+    func peekFront() -> T? { front?.value }
+    func peekBack() -> T? { back?.value }
+
+    mutating func popFront() -> T? {
+        guard let oldFront = front else { return nil }
+        if oldFront.next == nil {
+            front = nil
+            back = nil
+        } else {
+            oldFront.next!.previous = nil
+            front = oldFront.next!
+        }
+        return oldFront.value
+    }
+
+    mutating func popBack() -> T? {
+        guard let oldBack = back else { return nil }
+        if oldBack.previous == nil {
+            front = nil
+            back = nil
+        } else {
+            oldBack.previous!.next = nil
+            back = oldBack.previous!
+        }
+        return oldBack.value
+    }
+}
+
 internal protocol Fifoable: BidirectionalCollection {
     associatedtype Element
     init(_: ReversedCollection<Self>)
---
+++ Tests/GetSchwiftyTests/Fifo.swift
@@ -0,0 +1,118 @@
+import XCTest
+@testable import GetSchwifty
+
+final class DLinkedListTests: XCTestCase {
+    func test1() throws {
+        var ll = DLinkedList<Int>()
+
+        ll.pushBack(1)
+        XCTAssertEqual(ll.popFront(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushBack(1)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushBack(1)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertNil(ll.popBack())
+        XCTAssertNil(ll.popFront())
+    }
+
+    func test2() throws {
+        var ll = DLinkedList<Int>()
+
+        ll.pushBack(1)
+        ll.pushBack(2)
+        XCTAssertEqual(ll.popFront(), 1)
+        XCTAssertEqual(ll.popFront(), 2)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushBack(1)
+        ll.pushBack(2)
+        XCTAssertEqual(ll.popBack(), 2)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushBack(1)
+        ll.pushBack(2)
+        XCTAssertEqual(ll.popBack(), 2)
+        XCTAssertEqual(ll.popFront(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+    }
+
+        func test3() throws {
+        var ll = DLinkedList<Int>()
+
+        ll.pushBack(1)
+        ll.pushBack(2)
+        ll.pushBack(3)
+        XCTAssertEqual(ll.popFront(), 1)
+        XCTAssertEqual(ll.popBack(), 3)
+        XCTAssertEqual(ll.popBack(), 2)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+    }
+
+        func test11() throws {
+        var ll = DLinkedList<Int>()
+
+        ll.pushFront(1)
+        XCTAssertEqual(ll.popFront(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushFront(1)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushFront(1)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertNil(ll.popBack())
+        XCTAssertNil(ll.popFront())
+    }
+
+    func test12() throws {
+        var ll = DLinkedList<Int>()
+
+        ll.pushFront(1)
+        ll.pushFront(2)
+        XCTAssertEqual(ll.popFront(), 2)
+        XCTAssertEqual(ll.popFront(), 1)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushFront(1)
+        ll.pushFront(2)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertEqual(ll.popBack(), 2)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+
+        ll.pushFront(1)
+        ll.pushFront(2)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertEqual(ll.popFront(), 2)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+    }
+
+        func test13() throws {
+        var ll = DLinkedList<Int>()
+
+        ll.pushFront(1)
+        ll.pushFront(2)
+        ll.pushFront(3)
+        XCTAssertEqual(ll.popFront(), 3)
+        XCTAssertEqual(ll.popBack(), 1)
+        XCTAssertEqual(ll.popBack(), 2)
+        XCTAssertNil(ll.popFront())
+        XCTAssertNil(ll.popBack())
+    }
+}
