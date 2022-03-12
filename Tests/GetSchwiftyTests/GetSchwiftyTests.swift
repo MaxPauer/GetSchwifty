581f8b01de4b7722ea36456a6c7d3a37a00fdcba
sik <ksilbermann@gmx.net>
2022-03-12 07:02:55 +0100

    Run FizzBuzz test

--- Tests/GetSchwiftyTests/GetSchwiftyTests.swift
+++ Tests/GetSchwiftyTests/GetSchwiftyTests.swift
@@ -2,7 +2,31 @@ import XCTest
 @testable import GetSchwifty
 
 final class GetSchwiftyTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertEqual(getSchwifty(""), "")
+    func testFizzBuzz() throws {
+        func fizzBuzz(n: Int) -> AnyHashable {
+            let isDiv3 = n % 3 == 0
+            let isDiv5 = n % 5 == 0
+            if isDiv3 && isDiv5 {
+                return "FizzBuzz!"
+            }
+            if isDiv3 {
+                return "Fizz!"
+            }
+            if isDiv5 {
+                return "Buzz!"
+            }
+            return n
+        }
+
+        var i = 1
+        let fizzbuzz = try! String(contentsOf: URL(fileURLWithPath: "./Tests/fizzbuzz.rock"))
+        let x = MainEvalContext(input: fizzbuzz, stdout: {o throws in
+            let out = try XCTUnwrap(o as? AnyHashable)
+            XCTAssertEqual(out, fizzBuzz(n: i))
+            XCTAssert(i < 101)
+            i += 1
+        })
+
+        try! x.run()
     }
 }
